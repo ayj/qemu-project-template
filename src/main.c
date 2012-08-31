1dce8694982b771de7c99ecc6d7b63404c63d816
Jason Young <a.young.jason@gmail.com>
2012-08-31 10:58:23 -0700

    printf example

--- src/main.c
+++ src/main.c
@@ -1,26 +1,22 @@
 #include <common.h>
 
-bool g_running = true;
-uint32_t g_counter = 0;
+volatile unsigned int * const UART0DR = (unsigned int *)0x101f1000;
 
-uint32_t update_counter(uint32_t counter) 
+void print_uart0(const char *s)
 {
-    return ++counter;
+    while(*s != '\0') {
+        *UART0DR = (unsigned int)(*s);
+        s++;
+    }
 }
 
 void run(void)
 {
-    g_counter = update_counter(g_counter);
+    print_uart0("hello!\n");
 }
 
 void _main(void)
 {
-    one_reset();
-    two_reset();
-    three_reset();
-    four_reset();
-
-    while (g_running) {
+    while (true)
         run();
-    }
 }
